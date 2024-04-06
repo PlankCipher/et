33211f465f7c33e78e5da63778be3522dc66f72e
PlankCipher <plankcipher1@protonmail.com>
2024-04-06 14:11:36 +0200

    feat(rofi): add rofi custom mode for et
    Closes #3

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo cp et rofi_et dmenu_et /sbin/
+sudo cp et rofi_et rofi_et_mode dmenu_et /sbin/
 sudo mkdir -p /usr/share/wordlists/
 sudo cp wordlist.txt /usr/share/wordlists/et_wordlist.txt
